9b11c1a26d21f21868a4b53afcab4a6920461235
Keemperor <jacobotau@hotmail.com>
2021-11-27 15:49:36 -0500

    fixed time color issue

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,11 @@ var colorText = document.getElementById('colorText');
 
 time.textContent = humanReadable;
 
-/*function emea() {
+function emea() {
   var now = moment();
-  var twentyFour = now.format("hh:mm");
-  var time = moment() + 1;
- 
+    
+  var nineAM = moment('09:00', "HH:mm", true);
+  var tenAM = moment('10:00', "HH:mm", true);
   var nineAM = moment('09:00', "kk:mm", true);
   var tenAM = moment('10:00', "kk:mm", true);
   var elevenAM = moment('11:00', "HH:mm", true);
@@ -19,27 +19,74 @@ time.textContent = humanReadable;
   var threePM = moment('15:00', "HH:mm", true);
   var fourPM = moment('16:00', "HH:mm", true);
   var fivePM = moment('17:00', "HH:mm", true);
-  var test1 = moment('01:00', "HH:mm", true);
-  var test2 = moment('02:00', "HH:mm", true);
+  var sixPM = moment('18:00', "HH:mm", true);
+  
 
+  // var nineAM = moment('09:00AM', "hh:mmA", true);
+//console.log(now);
+//console.log(time);
+//console.log(nineAM);
 
-  if (now > nineAM || now < tenAM) {
-      // If time is after 6PM or before 9AM, apply night theme to 'body'
-      $("#colorText").addClass("present").removeClass("future");
-  } else {
-      // Else use ‘day’ theme
-      $("#colorText").addClass("past").removeClass("present").removeClass("future");
-  }
+ if (now > nineAM && now < tenAM) {
+    // If time is after 9AM or before 10AM, apply present theme to 'colorText'
+    $("#colorText").addClass("present").removeClass("future");
+} else if (now > nineAM) {
+    // Else use ‘past’ theme
+    $("#colorText").addClass("past").removeClass("future");
+}
 
-  if (time < tenAM || time > elevenAM) {
-    // If time is after 6PM or before 9AM, apply night theme to 'body'
-    $("#colorText1").addClass("present").removeClass("future");
-} else {
-    // Else use ‘day’ theme
-    $("#colorText1").addClass("past").removeClass("present").removeClass("future");
-}*/
+if (now > tenAM && now < elevenAM) {
+  $("#colorText1").addClass("present").removeClass("future");
+} else if (now > tenAM) {
+  $("#colorText1").addClass("past").removeClass("future");
+}
 
+if (now > elevenAM && now < twelvePM) {
+  $("#colorText2").addClass("present").removeClass("future");
+} else if (now > elevenAM) {
+  $("#colorText2").addClass("past").removeClass("future");
+}
 
+if (now > twelvePM && now < onePM) {
+  $("#colorText3").addClass("present").removeClass("future");
+} else if (now > twelvePM) {
+  $("#colorText3").addClass("past").removeClass("future");
+}
+
+if (now > onePM && now < twoPM) {
+  $("#colorText4").addClass("present").removeClass("future");
+} else if (now > onePM) {
+  $("#colorText4").addClass("past").removeClass("future");
+}
+
+if (now > twoPM && now < threePM) {
+  $("#colorText5").addClass("present").removeClass("future");
+} else if (now > twoPM) {
+  $("#colorText5").addClass("past").removeClass("future");
+}
+
+if (now > threePM && now < fourPM) {
+  $("#colorText6").addClass("present").removeClass("future");
+} else if (now > threePM) {
+  $("#colorText6").addClass("past").removeClass("future");
+}
+
+if (now > fourPM && now < fivePM) {
+  $("#colorText7").addClass("present").removeClass("future");
+} else if (now > fourPM) {
+  $("#colorText7").addClass("past").removeClass("future");
+}
+
+if (now > fivePM && now < sixPM) {
+  $("#colorText8").addClass("present").removeClass("future");
+} else if (now > fivePM) {
+  $("#colorText8").addClass("past").removeClass("future");
+}
+
+$(document).ready(function () {
+  emea();
+
+})};
 
 
 setInterval (() => {
@@ -70,6 +117,11 @@ console.log(nineAM);
 } else if (now > nineAM) {
     // Else use ‘past’ theme
     $("#colorText").addClass("past").removeClass("future");
+}
+
+if (now > nineAM) {
+  // Else use ‘past’ theme
+  $("#colorText").addClass("past").removeClass("future");
 }
 
 if (now > tenAM && now < elevenAM) {
