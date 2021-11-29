d9f864f8148c164ba3f27e7b50d06e85011145f8
Keemperor <jacobotau@hotmail.com>
2021-11-29 01:35:32 -0500

    save function created and comments added

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ if (now >= fivePM && now <= sixPM) {
 
 }, (1000 * 60) ); 
 
-tasks = [];
+/*tasks = [];
 
 //load tasks
 var loadTasks = function(){
@@ -190,7 +190,6 @@ var printTasks = function(){
     })
  }
 
- 
 //Task update with click
 $(".taskBin").on("click", "p", function(){
   // console.log("<p> was clicked");
@@ -224,12 +223,35 @@ $(".taskBin").on("blur", "textarea", function() {
 
 //Save tasks
 $(".saveBtn").on("click", function(){
-  //   console.log("<save button> was clicked");
+     console.log("<save button> was clicked");
     var index = $(".saveBtn").index(this);
   //   console.log(index)
     tasks[index] = $(this).parent().find(".taskItem").text();
     localStorage.setItem("tasks", JSON.stringify(tasks));
 });
 
+loadTasks();*/
 
-loadTasks();
+$(document).ready(function () {
+  // saveBtn click listener 
+  $(".saveBtn").on("click", function () {
+      // Get nearby values of the description in JQuery
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+
+      // Save text in local storage
+      localStorage.setItem(time, text);
+  })
+
+   // Get item from local storage if any
+   $("#hour9 .description").val(localStorage.getItem("hour9"));
+   $("#hour10 .description").val(localStorage.getItem("hour10"));
+   $("#hour11 .description").val(localStorage.getItem("hour11"));
+   $("#hour12 .description").val(localStorage.getItem("hour12"));
+   $("#hour13 .description").val(localStorage.getItem("hour13"));
+   $("#hour14 .description").val(localStorage.getItem("hour14"));
+   $("#hour15 .description").val(localStorage.getItem("hour15"));
+   $("#hour16 .description").val(localStorage.getItem("hour16"));
+   $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+})
